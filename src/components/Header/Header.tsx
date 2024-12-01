057315d84d298b190aa3f9aab949a32b69d65ace
Vladimir Novichikhin <darkonica@gmail.com>
2024-12-01 16:56:43 +0300

    basic layout

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+export const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        Shawarma shop
+      </div>
+      <menu>
+        <li>Меню</li>
+        <li>Доставка</li>
+        <li>О нас</li>
+        <li>Контакты</li>
+        <li>Корзина</li>
+      </menu>
+    </header>
+  )
+}
