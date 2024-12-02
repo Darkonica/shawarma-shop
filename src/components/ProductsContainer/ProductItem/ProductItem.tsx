1890667092ef8cdad63344a15e1dc5322cbc32a0
Vladimir Novichikhin <darkonica@gmail.com>
2024-12-02 07:52:30 +0300

    header sticky + product styling

--- src/components/ProductsContainer/ProductItem/ProductItem.tsx
+++ src/components/ProductsContainer/ProductItem/ProductItem.tsx
@@ -4,13 +4,13 @@ export const ProductItem = ({ title, image, description, price }) => {
   return (
     <div className={styles.productItem}>
       <img className={styles.image} src={image} alt="" />
-      <h3>{title}</h3>
+      <h3 className={styles.title}>{title}</h3>
 
       <div className={styles.description}>{description}</div>
 
       <div className={styles.priceCont}>
         <div className={styles.price}>{price}₽</div>
-        <button>В корзину</button>
+        <button className={styles.cart}>В корзину</button>
       </div>
     </div>
   )
