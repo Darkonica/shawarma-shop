2a8880e3c00ca685bf5383c9d386ca0a8e0a4633
Vladimir Novichikhin <darkonica@gmail.com>
2024-12-07 12:27:14 +0300

    navigation moved to footer

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,7 +1,21 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 import styles from './styles.module.scss';
 
 export const Footer = () => {
+  const pathname = usePathname();
+
   return (
-    <footer className={styles.footer}>Footer</footer>
+    <footer className={styles.footer}>
+      <div></div>
+      <nav>
+        <Link href="/delivery" className={pathname === '/delivery' ? 'active' : ''}>Доставка</Link>
+        <Link href="/about" className={pathname === '/about' ? 'active' : ''}>О нас</Link>
+        <Link href="/contacts" className={pathname === '/contacts' ? 'active' : ''}>Контакты</Link>
+      </nav>
+    </footer>
   )
 }
--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,13 +1,8 @@
-'use client';
-
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 
 import styles from './styles.module.scss';
 
 export const Header = () => {
-  const pathname = usePathname();
-
   return (
     <header className={styles.header}>
       <Link
@@ -16,12 +11,7 @@ export const Header = () => {
       >
         Shawarma shop
       </Link>
-      <nav>
-        <Link href="/delivery" className={pathname === '/delivery' ? 'active' : ''}>Доставка</Link>
-        <Link href="/about" className={pathname === '/about' ? 'active' : ''}>О нас</Link>
-        <Link href="/contacts" className={pathname === '/contacts' ? 'active' : ''}>Контакты</Link>
-        <Link href="/cart" className={pathname === '/cart' ? 'active' : ''}>Корзина</Link>
-      </nav>
+      <Link href="/cart">Корзина</Link>
     </header>
   )
 }
