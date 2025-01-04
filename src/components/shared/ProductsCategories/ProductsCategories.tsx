de464d81586c7d174e7fd3b0f481db529fd3b600
Vladimir Novichikhin <darkonica@gmail.com>
2025-01-04 10:18:40 +0300

    categories bar sticky

--- src/components/shared/ProductsCategories/ProductsCategories.tsx
+++ src/components/shared/ProductsCategories/ProductsCategories.tsx
@@ -2,14 +2,16 @@ import styles from './styles.module.scss';
 
 export const ProductsCategories = () => {
   return (
-    <div className={styles.wrapper}>
-      <ul>
-        <li className="active"><a href="#new-items">Новинки</a></li>
-        <li><a href="#shawerma">Шаверма</a></li>
-        <li><a href="#snacks">Закуски</a></li>
-        <li><a href="#sauces">Соусы</a></li>
-        <li><a href="#hot-drinks">Горячие напитки</a></li>
-      </ul>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <ul>
+          <li className="active"><a href="#new-items">Новинки</a></li>
+          <li><a href="#shawerma">Шаверма</a></li>
+          <li><a href="#snacks">Закуски</a></li>
+          <li><a href="#sauces">Соусы</a></li>
+          <li><a href="#hot-drinks">Горячие напитки</a></li>
+        </ul>
+      </div>
     </div>
   )
 }
